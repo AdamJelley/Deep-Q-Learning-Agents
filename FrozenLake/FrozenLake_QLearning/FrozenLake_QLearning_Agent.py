507cc014b20ed285bb987b174971070dd1d2d528
Adam Jelley <47476156+AdamJelley@users.noreply.github.com>
2020-11-10 08:05:14 +0000

    Initialise agent

---
+++ FrozenLake/FrozenLake_QLearning/FrozenLake_QLearning_Agent.py
@@ -0,0 +1,16 @@
+
+class Agent():
+    def __init__(self, lr, gamma, eps_start, eps_end, eps_dec, n_states, n_actions):
+        self.lr = lr
+        self.gamma = gamma
+        self.eps_max = eps_start
+        self.eps_min = eps_end
+        self.eps_dec = eps_dec
+
+        self.Q = {}
+        self._init_Q()
+
+    def _init_Q(self):
+        for state in range(self.n_states):
+            for action in range(self.n_actions):
+                self.Q[(state, action)] = 0.0
