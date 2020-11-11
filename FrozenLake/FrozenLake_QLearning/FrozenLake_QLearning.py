17a61c41e8f05a1b95a8192fad48a6cd8485f8c2
Adam Jelley <47476156+AdamJelley@users.noreply.github.com>
2020-11-11 20:27:21 +0000

    Implemented initial Q-learning agent and training

--- FrozenLake/FrozenLake_QLearning/FrozenLake_QLearning.py
+++ FrozenLake/FrozenLake_QLearning/FrozenLake_QLearning.py
@@ -3,26 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from FrozenLake_QLearning_Agent import Agent
 
-env = gym.make('FrozenLake-v0')
+if __name__ == "__main__":
 
-n_games = 1000
-win_pct = []
-scores = []
+    n_games = 500000
+    win_pct_list = []
+    scores = []
 
-agent = Agent(lr=0.001, gamma=0.9, eps_start=1.0, eps_end=0.01, eps_dec=0.01, n_states=16, n_actions=4)
+    env = gym.make('FrozenLake-v0')
+    agent = Agent(lr=0.001, gamma=0.9, eps_start=1.0, eps_end=0.01, eps_dec=0.999999, n_states=16, n_actions=4)
 
-for i in range(n_games):
-    done = False
-    obs = env.reset()
-    score = 0
-    while not done:
-        action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
-        score += reward
-    scores.append(score)
+    for i in range(n_games):
+        done = False
+        obs = env.reset()
+        score = 0
+        while not done:
+            state = obs
+            action = agent.choose_action(state)
+            obs, reward, done, info = env.step(action)
+            agent.learn(state, action, reward, obs)
+            score += reward
+        scores.append(score)
 
-    if i % 10 == 0:
-        average = np.mean(scores[-10:])
-        win_pct.append(average)
-plt.plot(win_pct)
-plt.show()
+        if i % 100 == 0:
+            win_pct = np.mean(scores[-100:])
+            win_pct_list.append(win_pct)
+            if i % 1000 == 0:
+                print(f"Episode {i}, win_pct {win_pct:.2f}, epsilon {agent.epsilon:.2f}")
+
+    plt.plot(win_pct_list)
+    plt.show()
--- FrozenLake/FrozenLake_QLearning/FrozenLake_QLearning_Agent.py
+++ FrozenLake/FrozenLake_QLearning/FrozenLake_QLearning_Agent.py
@@ -1,9 +1,10 @@
+import numpy as np
 
 class Agent():
     def __init__(self, lr, gamma, eps_start, eps_end, eps_dec, n_states, n_actions):
         self.lr = lr
         self.gamma = gamma
-        self.eps_max = eps_start
+        self.epsilon = eps_start
         self.eps_min = eps_end
         self.eps_dec = eps_dec
         self.n_states = n_states
@@ -17,4 +18,21 @@ class Agent():
             for action in range(self.n_actions):
                 self.Q[(state, action)] = 0.0
 
-    
+    def choose_action(self, state):
+        if np.random.random() > self.epsilon:
+            actions = np.array([self.Q[(state, action)] for action in range(self.n_actions)])
+            action = np.argmax(actions)
+        else:
+            action = np.random.choice(list(range(self.n_actions)))
+        return action
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon*self.eps_dec if self.epsilon > self.eps_min else self.eps_min
+
+    def learn(self, state, action, reward, new_state):
+        new_action = np.argmax(np.array([self.Q[(new_state, action)] for action in range(self.n_actions)]))
+        
+        self.Q[(state, action)] += self.lr*(reward + self.gamma*\
+                                (self.Q[(new_state, new_action)]-self.Q[(state, action)]))
+        
+        self.decrement_epsilon()
